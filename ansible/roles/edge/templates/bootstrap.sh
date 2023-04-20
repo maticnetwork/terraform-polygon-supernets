51b52d14654c45f59256d4203bb9501a89baebda
Jesse Lee <gatsbyz@users.noreply.github.com>
2023-04-20 16:26:51 -0400

    fix mint script to skip deploy and use predeployed address (#5)
    * fix mint script to skip deploy and use predeployed address
    
    * premine address as variables and adjust to latest version of edge

--- ansible/roles/edge/templates/bootstrap.sh
+++ ansible/roles/edge/templates/bootstrap.sh
@@ -31,36 +31,31 @@ main() {
                  --premine-validators 1000000000000000000000000000 \
                  --chain-id {{ chain_id }}
 
+    polygon-edge genesis \
+                {% for item in hostvars %}{% if (hostvars[item].tags.Role == "validator") %} --validators /dns4/{{ hostvars[item].tags["Name"] }}/tcp/{{ edge_p2p_port }}/p2p/$(cat {{ hostvars[item].tags["Name"] }}.json | jq -r '.[0].node_id'):$(cat {{ hostvars[item].tags["Name"] }}.json | jq -r '.[0].address'):$(cat {{ hostvars[item].tags["Name"] }}.json | jq -r '.[0].bls_pubkey'):$(cat {{ hostvars[item].tags["Name"] }}.json | jq -r '.[0].bls_signature') {% endif %}{% endfor %} \
+                 --consensus polybft \
+                 --grpc-address {{ rootchain_json_rpc }} \
+                 {% for item in hostvars %}{% if (hostvars[item].tags.Role == "fullnode" or hostvars[item].tags.Role == "validator") %} --bootnode /dns4/{{ hostvars[item].tags["Name"] }}/tcp/{{ edge_p2p_port }}/p2p/$(cat {{ hostvars[item].tags["Name"] }}.json | jq -r '.[0].node_id') {% endif %}{% endfor %} \
+                 {% for address in premine_address %} --premine {{ address }}:1000000000000000000000000000 {% endfor %}
+                 --premine 0x0000000000000000000000000000000000000000 \
+                 --block-gas-limit {{ block_gas_limit }} \
+                 --block-time {{ block_time }}s \
+                 --chain-id {{ chain_id }} \
+                 --epoch-size 10
+
     polycli wallet create --words 12 --language english | jq '.Addresses[0]' > rootchain-wallet.json
     COINBASE_ADDRESS=$(curl -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_coinbase","params":[],"id":1}' {{ rootchain_json_rpc }} | jq -r '.result')
     curl -X POST --data '{"jsonrpc":"2.0","method":"eth_sendTransaction","params":[{"from":"'"$COINBASE_ADDRESS"'","to":"'"$(cat rootchain-wallet.json | jq -r '.ETHAddress')"'","value":"0x3635C9ADC5DEA00000"}],"id":1}' -H "Content-Type: application/json" {{ rootchain_json_rpc }}
-
     sleep 5
-    polygon-edge rootchain init-contracts \
+    polygon-edge rootchain deploy \
       --deployer-key $(cat rootchain-wallet.json | jq -r '.HexPrivateKey') \
-      --json-rpc {{ rootchain_json_rpc }}
-      
+      --json-rpc {{ rootchain_json_rpc }}      
 
 {% for item in hostvars %}
 {% if (hostvars[item].tags.Role == "validator") %}
     polygon-edge rootchain fund --data-dir {{ hostvars[item].tags["Name"] }} --json-rpc {{ rootchain_json_rpc }}
 {% endif %}
 {% endfor %}
-
-    LATEST_BLOCK=$(curl -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}' {{ rootchain_json_rpc }} | jq -r '.result')
-
-    polygon-edge genesis \
-                 --consensus polybft \
-                 --bridge-json-rpc {{ rootchain_json_rpc }} \
-                 {% for item in hostvars %}{% if (hostvars[item].tags.Role == "fullnode" or hostvars[item].tags.Role == "validator") %} --bootnode /dns4/{{ hostvars[item].tags["Name"] }}/tcp/{{ edge_p2p_port }}/p2p/$(cat {{ hostvars[item].tags["Name"] }}.json | jq -r '.[0].node_id') {% endif %}{% endfor %} \
-                 --premine {{ loadtest_account }}:1000000000000000000000000000 \
-                 --premine 0x0000000000000000000000000000000000000000 \
-                 --tracker-start-blocks $(cat manifest.json | jq -r '.rootchain.stateSenderAddress'):$(echo $(($LATEST_BLOCK))) \
-                 --block-gas-limit {{ block_gas_limit }} \
-                 --block-time {{ block_time }}s \
-                 --chain-id {{ chain_id }} \
-                 --epoch-size 10 \
-                 --manifest ./manifest.json
 
     tar czf {{ base_dn }}.tar.gz validator* fullnode* genesis.json
     popd
--- ansible/roles/edge/templates/deploy-and-run.js
+++ ansible/roles/edge/templates/deploy-and-run.js
@@ -5,27 +5,18 @@ const data = JSON.parse(await fs.readFile('/var/lib/bootstrap/core-contracts/art
 
 const web3 = new Web3('{{ rootchain_json_rpc }}')
 
-
 const loadtestAccount = web3.eth.accounts.privateKeyToAccount('0x' + process.env.PRIVATE_KEY)
 console.log(loadtestAccount)
 
 const addedAccount = web3.eth.accounts.wallet.add(loadtestAccount);
 console.log('addedAccount', addedAccount);
 
-var contract = new web3.eth.Contract(data.abi);
+var contract = new web3.eth.Contract(data.abi, process.env.NATIVE_ERC20_ADDR); // provide a correct address here
 console.log('contract', contract);
 
-const deployed = await contract.deploy({
-  data: data.bytecode,
-  arguments: ['hello', 'bye']
-}).send({
-  gas: 10000000,
-  from: loadtestAccount.address
-})
-console.log('deployed', deployed);
-
-
-const mint = await deployed.methods.mint(loadtestAccount.address, web3.utils.toWei('1000000000000000000000', 'ether')).send({
+// value wasn't correct, because we are providing ether value and convert it into Wei
+// so if we want to convert 1000 ether to wei, just put 1000 (instead of 10^21) :) 
+const mint = await contract.methods.mint(loadtestAccount.address, web3.utils.toWei('1000', 'ether')).send({
   gas: 10000000,
   from: loadtestAccount.address
 })
