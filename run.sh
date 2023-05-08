6c2a366ddf117b1ebdc887960c162cfb5b9093c6
Jesse Lee <gatsby1118@gmail.com>
2023-05-08 15:33:43 -0400

    changes in bash script for script run

--- run.sh
+++ run.sh
@@ -15,10 +15,6 @@ ROOTCHAIN_RPC=$(terraform output -raw geth_private_ip)
 cd ansible
 ### Set ansible vault password
 echo "viable decrease resist spoil loop vocal foot only become glass satisfy dog pull junior jaguar maple entry donate panel slow innocent try movie snake" > password.txt
-### Create variable file from template
-cp local-extra-vars.yml.template local-extra-vars.yml
-### Add rootchain rpc address to the variable file
-echo "\nrootchain_json_rpc: http://$ROOTCHAIN_RPC:8545" >> local-extra-vars.yml
 
 ### Run ansible to configure the nodes
 ansible --inventory inventory/aws_ec2.yml --vault-password-file=password.txt --extra-vars "@local-extra-vars.yml" all -m ping
